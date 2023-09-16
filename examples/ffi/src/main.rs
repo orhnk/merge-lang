b0e14de08ed61b7f07d07e9ee30f47e3a8dea603
utfeight <utfeightt@gmail.com>
2023-09-16 06:09:40 +0300

    feat(exmpls/ffi/.rs): add rust & c communication

--- examples/ffi/src/main.rs
+++ examples/ffi/src/main.rs
@@ -1,49 +1,19 @@
-use std::{
-    fs::File,
-    io::{self, Write},
-    process::Command,
-};
+extern crate libc;
 
-#[derive(Debug)]
-struct Pipe<'a> {
-    data: &'a str,
-    ptr: File,
+use libc::c_char;
+use std::ffi::CStr;
+use std::str;
+
+extern "C" {
+    fn ccall() -> *const c_char;
 }
 
-impl<'a> Pipe<'a> {
-    fn new(data: &'a str) -> std::io::Result<Self> {
-        Ok(Pipe {
-            data,
-            ptr: File::create("examples/demo/src/pipe.json")?,
-        })
-    }
-
-    fn send(&mut self) -> io::Result<usize> {
-        Ok(self.ptr.write(self.data.as_bytes())?)
-    }
-}
-
-fn main() -> io::Result<()> {
-    println!("--------------- RUST ---------------");
-    let mut data = Pipe::new(
-        r#"
-{
-    "data": "(main.rs) Sent from rust."
-}
-"#,
-    )?;
-
-    println!("From Rust: {:#?}", data);
-    println!("Sending...");
-    data.send()?;
-    println!("Sent!");
-
-    Command::new("python")
-        .arg("examples/demo/src/pipe.py")
-        .spawn()
-        .expect("Couldn't run python. Make Sure you've installed it.");
-
-    println!("------------- END RUST -------------");
-    println!();
-    Ok(())
+fn main() {
+    println!("[Rust] Receiving message...");
+    let c_buf: *const c_char = unsafe { ccall() };
+    println!("[Rust] Got It!");
+    let c_str: &CStr = unsafe { CStr::from_ptr(c_buf) };
+    let str_slice: &str = c_str.to_str().unwrap();
+    let str_buf: String = str_slice.to_owned(); // if necessary
+    println!("[Rust] Message is: {}", str_buf);
 }
