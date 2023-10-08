70f9f5397cc337ff0c1d51ee5d1d0576f7df9f22
utfeight <utfeightt@gmail.com>
2023-10-08 18:15:50 +0300

    refactor: Lexer -> Tokenizer

--- core/src/tokenizer/tokenizer.rs
+++ core/src/tokenizer/tokenizer.rs
@@ -1,72 +1,65 @@
 use crate::tokenizer::Token;
 // Define a struct to represent the parser.
-// Define a struct to represent the lexer.
+// Define a struct to represent the tokenizer.
 #[derive(Debug, Eq, PartialEq)]
-pub struct Lexer {
+pub struct Tokenizer {
     input: String,   // The input string
     position: usize, // Current position in the input string
 }
 
 #[allow(dead_code)]
-impl Lexer {
-    // Constructor for the Lexer struct.
+impl Tokenizer {
+    // Constructor for the tokenizer struct.
     pub fn new(input: String) -> Self {
-        Lexer { input, position: 0 }
+        Tokenizer { input, position: 0 }
     }
 
-    // Helper function to advance the lexer's position.
+    /// Moves the cursor to the next character.
     pub fn advance(&mut self) {
         self.position += 1;
     }
 
-    pub fn get(&self) -> char {
+    /// Getter for the current character.
+    pub fn get_cursor_char(&self) -> char {
         // The ambiguidy is resolved by unwrap_or_else(|_| 0)
         self.input.chars().nth(self.pos()).unwrap() // for the sake of lazy evaluation
     }
 
-    pub fn index(&self, index: isize) -> char {
+    /// Offset the cursor by `index` and return the character.
+    pub fn offset(&self, index: isize) -> char {
         // The ambiguidy is resolved by unwrap_or_else(|_| 0)
         self.input
             .chars()
-            .nth((TryInto::<isize>::try_into(self.pos()).expect("usize & isize Overflow") as isize + index) as usize)
+            .nth(
+                (TryInto::<isize>::try_into(self.pos()).expect("usize & isize Overflow") as isize
+                    + index) as usize,
+            )
             .unwrap()
     }
 
+    /// Get the character at `index`.
     pub fn get_exact(&self, index: usize) -> char {
         // The ambiguidy is resolved by unwrap_or_else(|_| 0)
         self.input.chars().nth(index).unwrap() // for the sake of lazy evaluation
     }
 
+    /// Getter for the cursor position.
     pub fn pos(&self) -> usize {
         self.position
     }
 
-    pub fn check(&self, lit: &str) -> bool {
-        let mut index = 0isize;
-        let mut lit_chars = lit.chars();
-        for c in lit_chars {
-            if c != self.index(index) {
-                return false;
-            }
-            index += 1;
-        }
-        true
-    }
-
-    // Helper function to skip whitespace characters.
+    /// Helper function to skip whitespace characters.
     pub fn skip_whitespace(&mut self) {
-        // TODO: rename this function according to whitespaces_and_comments which I'm too lazy to do now.
         while let Some(c) = self.peek() {
             if c.is_whitespace() {
                 self.advance();
-            } else if c == '/' && self.get() == '/' {
             } else {
                 break;
             }
         }
     }
 
-    // Helper function to peek at the current character.
+    /// Helper function to peek at the current character.
     pub fn peek(&self) -> Option<char> {
         if self.position < self.input.len() {
             Some(self.input.chars().nth(self.position).unwrap()) // TODO: use unicode-segmentation
@@ -75,7 +68,7 @@ impl Lexer {
         }
     }
 
-    // Helper function to consume the current character.
+    /// Helper function to consume the current character.
     pub fn consume(&mut self) -> Option<char> {
         if self.position < self.input.len() {
             let c = self.input.chars().nth(self.position).unwrap();
@@ -86,7 +79,7 @@ impl Lexer {
         }
     }
 
-    // Tokenizes the input string.
+    /// Tokenizes the input string.
     pub fn tokenize(&mut self) -> Vec<Token> {
         let mut tokens = Vec::new();
         self.skip_whitespace(); // Skip initial whitespaces
@@ -146,3 +139,12 @@ impl Lexer {
     }
 }
 
+// struct TokenizerOpts {
+//     inline: bool,
+// }
+//
+// impl std::default::Default for TokenizerOpts {
+//     TokenizerOpts {
+//         inline: false,
+//     }
+// }
