db0866f95df006ab22296186e1ed3c84906db205
utfeight <utfeightt@gmail.com>
2023-10-08 06:36:40 +0300

    chore: migrate to file

--- core/src/main.rs
+++ core/src/main.rs
@@ -112,18 +112,6 @@ static CODE: &str = r#"
 
 // Recursive Descent Parser for a Simple Arithmetic Expression Grammar
 
-use std::cmp::PartialEq;
-use thiserror::Error;
-
-// Lexer + Parser Error
-#[derive(Error, Debug)]
-pub enum SyntaxError {
-    #[error("Invalid token: {0}")]
-    InvalidToken(String),
-    #[error("Expected: {0}, found: {1}")]
-    IncompleteToken(String, String),
-}
-
 // struct LexerOpts {
 //     inline: bool,
 //     foo: bool,
