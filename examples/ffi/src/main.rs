8f628b96478a5fc037d3541e51fe5a70da1e94ae
utfeight <utfeightt@gmail.com>
2023-10-14 07:40:05 +0300

    feat(exmpl): rust update

--- examples/ffi/src/main.rs
+++ examples/ffi/src/main.rs
@@ -1,11 +1,17 @@
-extern crate libc;
-
 use libc::c_char;
 use std::ffi::CStr;
 use std::str;
 
 extern "C" {
     fn ccall() -> *const c_char;
+}
+
+#[no_mangle]
+pub extern "C" fn rustcall() -> *const c_char {
+    let c_str = std::ffi::CString::new("[Rust] Hello sir").unwrap();
+    let ptr = c_str.as_ptr();
+    std::mem::forget(c_str);
+    ptr
 }
 
 fn main() {
@@ -16,4 +22,5 @@ fn main() {
     let str_slice: &str = c_str.to_str().unwrap();
     let str_buf: String = str_slice.to_owned(); // if necessary
     println!("[Rust] Message is: {}", str_buf);
+    println!("[Rust] Sending Message to Python");
 }
