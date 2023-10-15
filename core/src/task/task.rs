c54ea42c1f670503ca098b14eb1f3cd66ecdd125
utfeight <utfeightt@gmail.com>
2023-10-15 12:14:21 +0300

    feat(task): events for the merge compiler

--- core/src/task/task.rs
+++ core/src/task/task.rs
@@ -1,27 +1,33 @@
-struct TaskBuf {
-    tasks: Vec<Task>,
+// Task Management System used by Merge
+// Copyright © 2023 Merge. All Rights Reserved.
+
+pub trait Task {
+    fn new() -> Self
+    where
+        Self: Sized;
+    fn run(&self);
 }
 
-impl TaskBuf {
-    fn new() -> Self {
+pub struct TaskManager {
+    pub tasks: Vec<Box<dyn Task>>,
+}
+
+impl TaskManager {
+    pub fn new() -> Self {
         Self { tasks: Vec::new() }
     }
 
-    fn push(&mut self, task: Task) {
+    pub fn pop(&mut self, _task: Box<dyn Task>) {
+        self.tasks.pop();
+    }
+
+    pub fn push(&mut self, task: Box<dyn Task>) {
         self.tasks.push(task);
     }
 
-    fn pop(&mut self) -> Option<Task> {
-        self.tasks.pop()
+    pub fn run_all(&self) {
+        for task in self.tasks.iter() {
+            task.run();
+        }
     }
-
-    fn len(&self) -> usize {
-        self.tasks.len()
-    }
-}
-
-// Task implementation
-struct Task {
-    name: String,
-    task: Box<dyn Fn() -> ()>,
 }
