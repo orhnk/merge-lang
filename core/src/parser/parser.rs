07a2ed07c12a224c0b4994c7abf3cae858b593ad
utfeight <utfeightt@gmail.com>
2023-11-09 03:44:50 +0300

    chore: up

--- core/src/parser/parser.rs
+++ core/src/parser/parser.rs
@@ -1,114 +1,138 @@
-// use crate::Token;
-//
-// pub struct Parser {
-//     input: Vec<Token>, // The input tokens
-//     current: usize,    // Index of the current token
-// }
-//
-// impl Parser {
-//     // TODO: Add new and focus methods. (focus to change the content and reset the cursor)
-//     // Constructor for the Parser struct.
-//     pub fn from(input: Vec<Token>) -> Self {
-//         Parser {
-//             input,
-//             current: 0,
-//         }
-//     }
-//
-//     // Entry point for the parser. Parses and evaluates an expression.
-//     pub fn parse_expression(&mut self) -> f64 {
-//         self.parse_addition()
-//     }
-//
-//     // Parses addition and subtraction.
-//     pub fn parse_addition(&mut self) -> f64 {
-//         let mut result = self.parse_multiplication();
-//
-//         while let Some(token) = self.peek() {
-//             match token {
-//                 Token::Plus => {
-//                     self.consume(); // Consume the '+' token
-//                     result += self.parse_multiplication();
-//                 }
-//                 Token::Minus => {
-//                     self.consume(); // Consume the '-' token
-//                     result -= self.parse_multiplication();
-//                 }
-//                 _ => break,
-//             }
-//         }
-//
-//         result
-//     }
-//
-//     // Parses multiplication and division.
-//     pub fn parse_multiplication(&mut self) -> f64 {
-//         let mut result = self.parse_primary();
-//
-//         while let Some(token) = self.peek() {
-//             match token {
-//                 Token::Multiply => {
-//                     self.consume(); // Consume the '*' token
-//                     result *= self.parse_primary();
-//                 }
-//                 Token::Divide => {
-//                     self.consume(); // Consume the '/' token
-//                     let denominator = self.parse_primary();
-//                     if denominator != 0.0 {
-//                         result /= denominator;
-//                     } else {
-//                         panic!("Division by zero");
-//                     }
-//                 }
-//                 _ => break,
-//             }
-//         }
-//
-//         result
-//     }
-//
-//     // Parses primary expressions (numbers and parentheses).
-//     pub fn parse_primary(&mut self) -> f64 {
-//         if let Some(token) = self.peek() {
-//             match token {
-//                 #[allow(unused_mut)]
-//                 Token::Number(mut num) => {
-//                     // HERE HELP REMOVE THIS `MUT`
-//                     self.consume(); // Consume the number token
-//                     num
-//                 }
-//                 Token::LeftParenthesis => {
-//                     self.consume(); // Consume the '(' token
-//                     let result = self.parse_addition();
-//                     if let Some(token) = self.peek() {
-//                         if *token == Token::RightParenthesis {
-//                             self.consume(); // Consume the ')' token
-//                             return result;
-//                         }
-//                     }
-//                     panic!("Unmatched '('");
-//                 }
-//                 _ => panic!("Unexpected token"),
-//             }
-//         } else {
-//             panic!("Unexpected end of input");
-//         }
-//     }
-//
-//     // Helper function to peek at the current token.
-//     pub fn peek(&self) -> Option<&Token> {
-//         if self.current < self.input.len() {
-//             Some(&self.input[self.current])
-//         } else {
-//             None
-//         }
-//     }
-//
-//     // Helper function to consume the current token.
-//     pub fn consume(&mut self) {
-//         if self.current < self.input.len() {
-//             self.current += 1;
-//         }
-//     }
-// }
-//
+use crate::Token;
+
+use super::error::ParserError;
+
+pub struct Parser {
+    tokens: Vec<Token>, // The input tokens
+    cursor: usize,      // Index of the current token
+}
+
+impl Parser {
+    // todo: add new and focus methods. (focus to change the content and reset the cursor)
+    // Constructor for the Parser struct.
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Parser { tokens, cursor: 0 }
+    }
+
+    // Entry point for the parser. Parses and evaluates an expression.
+    pub fn parse(&mut self) -> Result<(), Vec<ParserError>> {
+        self.parse_config()
+        // ![
+        //      <CONFIG OPTS>
+        // ]
+    }
+
+    pub fn parse_config(&mut self) -> Result<(), Vec<ParserError>> {
+        if let Some(Token::ExclamationMark) = self.peek() {
+            self.advance();
+            self.parse_config_opts()?;
+            Ok(())
+        } else {
+            Err(vec![ParserError::UnexpectedToken(Token::ExclamationMark)])
+        }
+    }
+
+    /// Parses the config options.
+    /// Given above in the example, this would be:
+    ///
+    /// ![
+    ///     rust = {
+    ///        build = "hello world",
+    ///     },
+    /// ]
+    pub fn parse_config_opts(&mut self) -> Result<(), Vec<ParserError>> {
+        // ![
+        //     rust = {
+        //         build = "hello world",
+        //     },
+        // ]
+
+        let mut curly_bracket_count = Vec::<Token>::new();
+        let mut square_bracket_coun = Vec::<Token>::new();
+
+        let cursor_token = self.peek();
+
+        println!("Cursor Token: {:?}", cursor_token);
+
+        match cursor_token {
+            Some(Token::LeftSquareBracket) => {
+                square_bracket_coun.push(Token::LeftSquareBracket);
+                self.advance();
+                self.parse_config_opts()?;
+                Ok(())
+            }
+
+            Some(Token::RightSquareBracket) => {
+                if square_bracket_coun.len() > 0 {
+                    square_bracket_coun.pop();
+                } else {
+                    return Err(vec![ParserError::UnexpectedToken({
+                        cursor_token.expect("Unexpected Token").clone() // FIXME: clone
+                    })]);
+                }
+                self.advance();
+                Ok(())
+            }
+
+            Some(Token::Identifier(_)) => {
+                self.parse_config_opts()?;
+                Ok(())
+            }
+
+            Some(Token::String(s)) => {
+                println!("Config String: {}", s);
+                self.parse_config_opts()?;
+                Ok(())
+            }
+
+            Some(Token::EqualSign) => {
+                self.advance();
+                self.parse_config_opts()?;
+                Ok(())
+            }
+
+            Some(Token::LeftCurlyBracket) => {
+                self.advance();
+                self.parse_config_opts()?;
+                Ok(())
+            }
+
+            Some(Token::RightCurlyBracket) => {
+                self.advance();
+                self.parse_config_opts()?;
+                Ok(())
+            }
+
+            // Enforce comma separation
+            Some(Token::Comma) => {
+                self.advance();
+                self.parse_config_opts()?;
+                Ok(())
+            }
+
+            Some(Token::Identifier(_)) => {
+                self.parse_config_opts()?;
+                Ok(())
+            }
+            
+            _ => Err(vec![ParserError::UnexpectedToken({
+                cursor_token.expect("Unexpected Token").clone() // FIXME: clone
+            })]),
+        }
+    }
+
+    pub fn peek(&self) -> Option<&Token> {
+        if self.cursor < self.tokens.len() {
+            Some(&self.tokens[self.cursor])
+        } else {
+            None
+        }
+    }
+
+    // Helper function to consume the current token.
+    pub fn advance(&mut self) {
+        if self.cursor < self.tokens.len() {
+            self.cursor += 1;
+        }
+    }
+}
